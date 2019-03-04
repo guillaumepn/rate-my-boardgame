c7aa06dff12bda73b25510588af92acd4674c365
guillaumepn <guillaumepn@free.fr>
2019-03-04 17:20:24 +0100

    auth jwt

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Models
+const User = require('./models/user');
 const Game = require('./models/game');
 const Rating = require('./models/rating');
 
+const verifyToken = require('./middlewares/security');
+
 const app = express();
+
+app.use(verifyToken);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const userRouter = require('./routes/user');
 const gameRouter = require('./routes/game');
@@ -16,26 +24,27 @@ app.use('/ratings', ratingRouter);
 
 app.listen(3000, () => console.log('listening'));
 
-// const user = new User();
-// user.userName = 'jojo';
-// user.save()
-//     .then(() => console.log("saved user"))
-//     .catch(error => console.log(error));
-//
-// User.find().then(items => console.log('users', items)).catch(error => console.log(error));
-//
-// const game = new Game();
-// game.title = 'Monopoly';
-// game.save()
-//     .then(() => console.log("saved game"))
-//     .catch(error => console.log(error));
-//
-// Game.find().then(items => console.log('games', items)).catch(error => console.log(error));
-//
-// const rating = new Rating();
-// rating.score = 8;
-// rating.save()
-//     .then(() => console.log("saved rating"))
-//     .catch(error => console.log(error));
-//
-// Rating.find().then(items => console.log('ratings', items)).catch(error => console.log(error));
+const user = new User();
+user.userName = 'jojo';
+user.save()
+    .then(() => console.log("saved user"))
+    .catch(error => console.log(error));
+
+User.find().then(items => console.log('users', items)).catch(error => console.log(error));
+
+const game = new Game();
+game.title = 'Tricot';
+game.year = 2018;
+game.save()
+    .then(() => console.log("saved game"))
+    .catch(error => console.log(error));
+
+Game.find().then(items => console.log('games', items)).catch(error => console.log(error));
+
+const rating = new Rating();
+rating.score = 8;
+rating.save()
+    .then(() => console.log("saved rating"))
+    .catch(error => console.log(error));
+
+Rating.find().then(items => console.log('ratings', items)).catch(error => console.log(error));
--- server/routes/game.js
+++ server/routes/game.js
@@ -4,10 +4,37 @@ const Game = require('../models/game');
 
 const router = express.Router();
 
-module.exports = router.get('/', (req, res) => {
+
+// Routes GET
+
+router.get('/', (req, res) => {
     console.log('route games');
     Game.find(req.query)
         .then(data => {
-        res.status(200).send(data);
-    })
+            res.status(200).send(data);
+        })
 });
+
+router.get('/:year', (req, res) => {
+    Game.find({year: req.params.year})
+        .then(data => {
+            res.status(200).send(data);
+        })
+});
+
+router.get('/:publisher', (req, res) => {
+    Game.find({publisher: req.params.publisher})
+        .then(data => {
+            res.status(200).send(data);
+        })
+});
+
+
+// Routes POST
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send();
+});
+
+module.exports = router;
