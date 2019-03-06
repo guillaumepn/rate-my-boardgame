3b0cc2ef0a280b9c0bf7e0434b075c98cb102010
guillaumepn <guillaumepn@free.fr>
2019-03-06 09:55:53 +0100

    add react + fix db volume

--- server/index.js
+++ server/index.js
@@ -26,27 +26,28 @@ app.use('/login_check', tokenRouter);
 
 app.listen(3000, () => console.log('listening'));
 
-// const user = new User();
-// user.userName = 'jojo';
-// user.save()
-//     .then(() => console.log("saved user"))
-//     .catch(error => console.log(error));
-//
-// User.find().then(items => console.log('users', items)).catch(error => console.log(error));
-//
-// const game = new Game();
-// game.title = 'Tricot';
-// game.year = 2018;
-// game.save()
-//     .then(() => console.log("saved game"))
-//     .catch(error => console.log(error));
-//
-// Game.find().then(items => console.log('games', items)).catch(error => console.log(error));
-//
-// const rating = new Rating();
-// rating.score = 8;
-// rating.save()
-//     .then(() => console.log("saved rating"))
-//     .catch(error => console.log(error));
-//
-// Rating.find().then(items => console.log('ratings', items)).catch(error => console.log(error));
+const user = new User();
+user.userName = 'jojo';
+user.save()
+    .then(() => console.log("saved user"))
+    .catch(error => console.log(error));
+
+User.find().then(items => console.log('users', items)).catch(error => console.log(error));
+
+const game = new Game();
+game.title = 'Tricot';
+game.year = 2018;
+game.save()
+    .then(() => console.log("saved game"))
+    .catch(error => console.log(error));
+
+Game.find().then(items => console.log('games', items)).catch(error => console.log(error));
+
+const rating = new Rating();
+rating.score = 8;
+rating.save()
+    .then(() => console.log("saved rating"))
+    .catch(error => console.log(error));
+
+Rating.find().then(items => console.log('ratings', items)).catch(error => console.log(error));
+
