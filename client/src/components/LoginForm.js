003605013c3c4f03a0e6c6761ee25e68bb5e2697
guillaumepn <guillaumepn@free.fr>
2019-03-08 00:24:11 +0100

    make stateless components functional

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,25 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Button, Form} from "semantic-ui-react";
 
-class LoginForm extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Connexion</h2>
-                <Form onSubmit={this.props.onSubmit}>
-                    <Form.Field required>
-                        <label htmlFor="username">Identifiant</label>
-                        <input type="text" id="username" onChange={(event) => this.props.onChange(event.currentTarget.value, 'username')}/>
-                    </Form.Field>
-                    <Form.Field required>
-                        <label htmlFor="password">Mot de passe</label>
-                        <input type="password" id="password" onChange={(event) => this.props.onChange(event.currentTarget.value, 'password')}/>
-                    </Form.Field>
-                    <Button type="submit">Se connecter</Button>
-                </Form>
-            </div>
-        );
-    }
-}
+const LoginForm = (props) => {
+    return (
+        <div>
+            <h2>Connexion</h2>
+            <Form onSubmit={props.onSubmit}>
+                <Form.Field required>
+                    <label htmlFor="username">Identifiant</label>
+                    <input type="text" id="username"
+                           onChange={(event) => props.onChange(event.currentTarget.value, 'username')}/>
+                </Form.Field>
+                <Form.Field required>
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="password" id="password"
+                           onChange={(event) => props.onChange(event.currentTarget.value, 'password')}/>
+                </Form.Field>
+                <Button type="submit">Se connecter</Button>
+            </Form>
+        </div>
+    );
+};
 
 export default LoginForm;
--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,27 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Button, Form} from "semantic-ui-react";
 
-class RegisterForm extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Inscription</h2>
-                <Form onSubmit={this.props.onSubmit}>
-                    <Form.Field required>
-                        <label htmlFor="username__register">Identifiant</label>
-                        <input type="text" id="username__register"
-                               onChange={(event) => this.props.onChange(event.currentTarget.value, 'username')}/>
-                    </Form.Field>
-                    <Form.Field required>
-                        <label htmlFor="password__register">Mot de passe</label>
-                        <input type="password" id="password__register"
-                               onChange={(event) => this.props.onChange(event.currentTarget.value, 'password')}/>
-                    </Form.Field>
-                    <Button type="submit">S'inscrire</Button>
-                </Form>
-            </div>
-        );
-    }
-}
+const RegisterForm = (props) => {
+    return (
+        <div>
+            <h2>Inscription</h2>
+            <Form onSubmit={props.onSubmit}>
+                <Form.Field required>
+                    <label htmlFor="username__register">Identifiant</label>
+                    <input type="text" id="username__register"
+                           onChange={(event) => props.onChange(event.currentTarget.value, 'username')}/>
+                </Form.Field>
+                <Form.Field required>
+                    <label htmlFor="password__register">Mot de passe</label>
+                    <input type="password" id="password__register"
+                           onChange={(event) => props.onChange(event.currentTarget.value, 'password')}/>
+                </Form.Field>
+                <Button type="submit">S'inscrire</Button>
+            </Form>
+        </div>
+    );
+};
 
 export default RegisterForm;
